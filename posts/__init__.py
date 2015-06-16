f1baa83d1788df19f7777db363cd0c0edf9013c6
Des Penny <despenny@gmail.com>
2015-06-16 12:51:06 +0000

    Added max file size to py and html, 1.1mb still crashes app

--- posts/__init__.py
+++ posts/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "posts.config.DevelopmentConfig")
 app.config.from_object(config_path)
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
 
 import api
 import views
--- posts/config.py
+++ posts/config.py
@@ -2,10 +2,10 @@ class DevelopmentConfig(object):
     DATABASE_URI = "postgresql://action:action@localhost:5432/posts"
     DEBUG = True
     UPLOAD_FOLDER = "uploads"
-    ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'bmp'])
+    
 
 class TestingConfig(object):
     DATABASE_URI = "postgresql://action:action@localhost:5432/posts-test"
     DEBUG = True
     UPLOAD_FOLDER = "test-uploads"
-    ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'bmp'])
+    
