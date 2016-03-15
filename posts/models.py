f71c48a0a464f710313cd83f0560e81594425275
despenny <despenny@gmail.com>
2016-03-15 10:52:09 +0000

    Generate tokens

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, String, Sequence, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Sequence, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 import datetime
 from database import Base
 from werkzeug.utils import secure_filename
 from flask.ext.login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 class User(Base, UserMixin):
     __tablename__ = "users"
@@ -13,16 +14,35 @@ class User(Base, UserMixin):
     email = Column(String(128), unique=True)
     password = Column(String(128))
     posts = relationship("Post", backref="author")
+    confirmed = Column(Boolean, default=False)
+    
     
     def as_dictionary(self):
         user = {
             "id": self.id,
             "name": self.name,
             "email": self.email,
-            "password":self.password
+            "password":self.password,
+            "confirmed":self.confirmed
             
         }
         return user
+    
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer('somesecretkey', expiration)
+        return s.dumps({'confirm':self.id})
+        
+    def confirm(self,token):
+        s = Serializer('somesecretkey')
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        add(self)
+        return True
     
 class Post(Base):
     __tablename__ = "posts"
