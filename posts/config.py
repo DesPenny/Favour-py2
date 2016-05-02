7cb7e76d80fb56a4f37a00554af31932824a82b7
despenny <despenny@gmail.com>
2016-05-02 12:30:55 +0000

    added email config but cannot test from c9

--- posts/config.py
+++ posts/config.py
@@ -1,3 +1,18 @@
+import os
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FAVOURSOUP_MAIL_SUBJECT_PREFIX = '[FavourSoup]'
+    FAVOURSOUP_MAIL_SENDER = 'FavourSoup Admin <favoursoup@gmail.com>'
+    FAVOURSOUP_ADMIN = os.environ.get('FavourSoup_ADMIN')
+
 class DevelopmentConfig(object):
     DATABASE_URI = "postgresql://action:action@localhost:5432/posts"
     DEBUG = True
